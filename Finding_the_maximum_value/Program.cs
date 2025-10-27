65e9b03b6fd3a0ef2e83fb3dccf93a876d7db6a6
selss61 <ofluberat33@gmail.com>
2025-10-27 18:16:57 +0300

    commit

---
+++ Finding_the_maximum_value/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+           
+
+        
+             int[] myArray = { 445, 5667, 45, 54, 78, 4593583, 457, 45, 78, 98, 1000 };
+            int MaxNumber = myArray[0];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] > MaxNumber)
+                {
+                    MaxNumber = myArray[i];
+                }
+
+
+            }
+            Console.WriteLine("Dizideki en büyük sayı:" + MaxNumber);
+            int sum = 0;
+            int[] number = new int[5];
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine($"Lütfen {i + 1}. sayıyı giriniz:");
+                number[i] = int.Parse(Console.ReadLine());
+                sum += number[i]; // sum = sum + number[i]; bu iki ifade aynı anlama gelir
+            }
+            Console.WriteLine("Girmiş olduğunuz sayıların toplamı:");
+            Console.WriteLine(sum);
+
+
+            
+
+
+
+            Console.Read();
+
+
+            
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
